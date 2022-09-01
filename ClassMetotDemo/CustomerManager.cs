7922464bde1e6d378583be98d35085e689ababd2
barisyaras <ismayaras@hotmail.com>
2022-09-01 06:28:29 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        List<Customer> customerList = new List<Customer>();
+
+        public void AddCustomer(string name, string surname)
+        {
+            Customer customer = new Customer();
+            customer.Name = name;
+            customer.Surname = surname;
+            customer.Id = GetLastCustomerId() + 1;
+            customerList.Add(customer);
+        }
+
+        public void ShowCustomers()
+        {
+            foreach (var item in customerList)
+            {
+                var customerInfo = $" {item.Id} {item.Name} {item.Surname}";
+                Console.WriteLine("Customers : " + customerInfo);
+                Console.WriteLine("Total number of customers is " + (customerList.Count));
+                Console.WriteLine("");
+            }
+        }
+
+        public bool DeleteCustomer(int Id)
+        {
+            int itemIndex = -1;
+            foreach (var item in customerList)
+            {
+                if (item.Id == Id)
+                {
+                    itemIndex = customerList.IndexOf(item);
+                    break;
+                }
+            }
+
+            if (itemIndex >= 0)
+            {
+                customerList.RemoveAt(itemIndex);
+                return true;
+            }
+            return false;
+        }
+
+        private int GetLastCustomerId()
+        {
+            int lastId = 0;
+
+            if (customerList != null && customerList.Any())
+            {
+                foreach (var item in customerList)
+                {
+                    if (lastId < item.Id)
+                        lastId = item.Id;
+                }
+            }
+
+            return lastId;
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isContinue = true;
+
+            CustomerManager customerManager = new CustomerManager();
+
+            while (isContinue)
+            {
+                int process = Menu();
+
+
+                switch (process)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter name of customer");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Please enter surname of customer");
+                        string surname = Console.ReadLine();
+                        customerManager.AddCustomer(name, surname);
+                        break;
+
+                    case 2:
+                        customerManager.ShowCustomers();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Please enter the Id of which customer do you want to delete");
+                        int sd = Convert.ToInt32(Console.ReadLine());
+                        customerManager.DeleteCustomer(sd);
+                        break;
+                    case 4:
+                        isContinue = false;
+                        break;
+
+
+                }
+
+
+                static int Menu()
+                {
+                    Console.WriteLine("Which of them do you want process");
+                    Console.WriteLine("1: Add customer");
+                    Console.WriteLine("2: List customers");
+                    Console.WriteLine("3: Delete customer");
+                    Console.WriteLine("4: Exit");
+                    int process = Convert.ToInt32(Console.ReadLine());
+                    return process;
+                }
+            }
+        }
+    }
+}
